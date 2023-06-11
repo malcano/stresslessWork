e8a366a12c588d2d94344d43b6783bc7352b24f1
malcano <steveleemh@naver.com>
2023-06-11 16:47:57 +0900

    init spring boot

---
+++ flowerHead/flowerHead/src/main/java/com/flowerHead/flowerHead/FlowerHeadApplication.java
@@ -0,0 +1,13 @@
+package com.flowerHead.flowerHead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerHeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerHeadApplication.class, args);
+	}
+
+}
---
+++ flowerHead/flowerHead/src/test/java/com/flowerHead/flowerHead/FlowerHeadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowerHead.flowerHead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerHeadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
